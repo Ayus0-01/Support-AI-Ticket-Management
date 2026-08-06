62ea79ff6491916502c473c5440658d8e332b368
Ayus0-01 <offayusmantripathy@gmail.com>
2026-08-06 16:30:38 +0530

    Fixed Hosts

--- server/AIticket/settings.py
+++ server/AIticket/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "https://support-ai-ticket-management-team-18k9.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
